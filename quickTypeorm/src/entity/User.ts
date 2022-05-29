97dcdeeb1d6b9e3ed7cdaa95d9fe04f796f2b06c
André Pereira <andrepereira97@yahoo.com>
2022-05-29 18:52:15 -0300

    :sparkles: add quick setup from typeorm docs

---
+++ quickTypeorm/src/entity/User.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  age: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
