d1700cdfe72d07facd680f568d74bb6c00518a3b
André Pereira <andrepereira97@yahoo.com>
2022-05-29 19:09:48 -0300

    :sparkles: add dotenv and change the configs of the database for that

---
+++ typeorm/ormconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  entities: ['src/models/**/*.ts'],
+  migrations: ['src/migrations/**/*.ts'],
+  cli: {
+    migrationsDir: ['src/migrations/'],
+    entitiesDir: 'src/models',
+  },
+};
