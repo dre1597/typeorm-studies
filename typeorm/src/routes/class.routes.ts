3d4427c7082695c0b276252e4176e45f7c84131e
André Pereira <andrepereira97@yahoo.com>
2022-05-29 19:35:53 -0300

    :sparkles: add show class by name and add some try catches blocks

--- typeorm/src/routes/class.routes.ts
+++ typeorm/src/routes/class.routes.ts
@@ -13,14 +13,42 @@ classRouter.post('/', async (request: Request, response: Response) => {
 
     return response.status(201).json(classSaved);
   } catch (error: any) {
-    console.log('error message', error.message);
+    console.log(
+      'There is an error on create a new class method',
+      error.message,
+    );
   }
 });
 
 classRouter.get('/', async (_request: Request, response: Response) => {
-  const classRepository = getRepository(Class);
+  try {
+    const classRepository = getRepository(Class);
 
-  const classes = await classRepository.find();
+    const classes = await classRepository.find();
 
-  return response.json(classes);
+    return response.json(classes);
+  } catch (error: any) {
+    console.log('There is an error on list all classes method', error.message);
+  }
+});
+
+classRouter.get('/:name', async (request: Request, response: Response) => {
+  try {
+    const classRepository = getRepository(Class);
+
+    const name = request.params.name;
+
+    const classFound = await classRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return response.json(classFound);
+  } catch (error: any) {
+    console.log(
+      'There is an error on list one class by name method',
+      error.message,
+    );
+  }
 });
